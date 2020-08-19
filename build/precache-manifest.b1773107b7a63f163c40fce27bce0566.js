1714ad7336b9193b4d71bc10a1f5af08aa5ec023
Thomas Lu <thomas@Thomass-MacBook-Air.local>
2020-08-19 05:20:58 -0700

    prelim for prod

---
+++ build/precache-manifest.b1773107b7a63f163c40fce27bce0566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdda7fac443f3429decfcfd3666afd63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e02d29d17bf42c9d28ae",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "c1108ccfc542a0db8fc4",
+    "url": "/static/css/main.1515b742.chunk.css"
+  },
+  {
+    "revision": "e02d29d17bf42c9d28ae",
+    "url": "/static/js/2.477d83e7.chunk.js"
+  },
+  {
+    "revision": "c1108ccfc542a0db8fc4",
+    "url": "/static/js/main.cae98032.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const mysqlConnection = require("./connection");
 const PeopleRoutes = require("./routes/people");
@@ -10,10 +11,16 @@ const cors = require("cors");
 var app = express();
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use(bodyParser.json());
 
 app.use("/people", PeopleRoutes);
 app.use("/events", EventsRoutes);
 app.use("/general/", GeneralRoutes);
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(3001);
